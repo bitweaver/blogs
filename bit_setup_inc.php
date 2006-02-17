93be606224d129f0d946ce25503b8f31efa6e277
Max Kremmel <xing@synapse.plus.com>
2006-02-17 23:51:40 +0000

    registerPackage now takes a hash instead of multiple parameters - keeps # of parameters under control

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -4,7 +4,12 @@
  */
 
 global $gBitSystem, $gBitUser, $gBitSmarty;
-$gBitSystem->registerPackage( 'blogs', dirname( __FILE__).'/' );
+
+$registerHash = array(
+	'package_name' => 'blogs',
+	'package_path' => dirname( __FILE__ ).'/',
+);
+$gBitSystem->registerPackage( $registerHash );
 
 if( $gBitSystem->isPackageActive( 'blogs' ) ) {
 	if( $gBitUser->hasPermission( 'bit_p_blog_admin' ) ) {
@@ -19,18 +24,18 @@ if( $gBitSystem->isPackageActive( 'blogs' ) ) {
 
 	$gBitSystem->registerNotifyEvent( array( "blog_post" => tra("An entry is posted to a blog") ) );
 
-    $gBitSmarty->assign('home_blog', 0);
-    $gBitSmarty->assign('blog_spellcheck', 'n');
-    $gBitSmarty->assign('blog_list_order', 'created_desc');
-    $gBitSmarty->assign('blog_list_title', 'y');
-    $gBitSmarty->assign('blog_list_description', 'y');
-    $gBitSmarty->assign('blog_list_created', 'y');
-    $gBitSmarty->assign('blog_list_lastmodif', 'y');
-    $gBitSmarty->assign('blog_list_user', 'y');
-    $gBitSmarty->assign('blog_list_posts', 'y');
-    $gBitSmarty->assign('blog_list_visits', 'y');
-    $gBitSmarty->assign('blog_list_activity', 'y');
-    $gBitSmarty->assign('blog_list_user', 'text');
+	$gBitSmarty->assign('home_blog', 0);
+	$gBitSmarty->assign('blog_spellcheck', 'n');
+	$gBitSmarty->assign('blog_list_order', 'created_desc');
+	$gBitSmarty->assign('blog_list_title', 'y');
+	$gBitSmarty->assign('blog_list_description', 'y');
+	$gBitSmarty->assign('blog_list_created', 'y');
+	$gBitSmarty->assign('blog_list_lastmodif', 'y');
+	$gBitSmarty->assign('blog_list_user', 'y');
+	$gBitSmarty->assign('blog_list_posts', 'y');
+	$gBitSmarty->assign('blog_list_visits', 'y');
+	$gBitSmarty->assign('blog_list_activity', 'y');
+	$gBitSmarty->assign('blog_list_user', 'text');
 
 }
 
