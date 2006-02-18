e3632e8a491b1a99b8374e346d051a9374fadddf
bitweaver.org <bitweaver@users.sourceforge.net>
2006-02-18 20:36:22 +0000

    adjust for module_rows

--- modules/mod_top_visited_blogs.php
+++ modules/mod_top_visited_blogs.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_blogs/modules/mod_top_visited_blogs.php,v 1.4 2006/02/03 17:23:54 squareing Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_blogs/modules/mod_top_visited_blogs.php,v 1.5 2006/02/18 20:36:22 bitweaver Exp $
  * @package blogs
  * @subpackage modules
  */
@@ -14,7 +14,7 @@ require_once( USERS_PKG_PATH.'BitUser.php' );
 global $gBlog, $gQueryUserId, $gBitThemes;
 
 $params = $gBitThemes->getModuleParameters('bitpackage:blogs/mod_top_visited_blogs.tpl', $gQueryUserId);
-$ranking = $gBlog->list_blogs(0, $params['rows'], 'hits_desc', '',$gQueryUserId,' `hits` IS NOT NULL ');
+$ranking = $gBlog->list_blogs(0, $params['module_rows'], 'hits_desc', '',$gQueryUserId,' `hits` IS NOT NULL ');
 
 $gBitSmarty->assign('modTopVisitedBlogs', $ranking["data"]);
 $gBitSmarty->assign('bulletSrc', isset($params["bullet"]) ? $params['bullet'] : NULL);
