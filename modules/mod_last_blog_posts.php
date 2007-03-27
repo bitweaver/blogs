e27c9a62df9424f8544b25653fc1e83a35a91886
wjames5 <will@tekimaki.com>
2007-03-27 21:54:30 +0000

    clean up last blog posts module

--- modules/mod_last_blog_posts.php
+++ modules/mod_last_blog_posts.php
@@ -3,7 +3,7 @@
  * Params: 
  * - title : if is "title", show the title of the post, else show the date of creation
  *
- * @version $Header: /cvsroot/bitweaver/_bit_blogs/modules/mod_last_blog_posts.php,v 1.6 2006/02/25 17:11:38 spiderr Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_blogs/modules/mod_last_blog_posts.php,v 1.7 2007/03/27 21:54:30 wjames5 Exp $
  * @package blogs
  * @subpackage modules
  */
@@ -15,11 +15,12 @@ if( !defined( 'MAX_BLOG_PREVIEW_LENGTH' ) ) {
 	define ('MAX_BLOG_PREVIEW_LENGTH', 100);
 }
 
-include_once( BLOGS_PKG_PATH.'BitBlog.php' );
+//DEPRECATED Slated for removal -wjames5
+//include_once( BLOGS_PKG_PATH.'BitBlog.php' );
 include_once( BLOGS_PKG_PATH.'BitBlogPost.php' );
 require_once( USERS_PKG_PATH.'BitUser.php' );
 
-global $gBitSmarty, $gBlog, $gQueryUserId, $module_rows, $module_params, $gBitSystem;
+global $gBitSmarty, $gQueryUserId, $module_rows, $module_params, $gBitSystem;
 
 $listHash = array( 'user_id' => $gQueryUserId, 'sort_mode' => 'created_desc', 'max_records' => $module_rows, 'parse_data' => TRUE );
 $blogPost = new BitBlogPost();
@@ -29,12 +30,14 @@ $gBitThemes = new BitThemes();
 $modParams = $gBitThemes->getModuleParameters('bitpackage:blogs/mod_last_blog_posts.tpl', $gQueryUserId);
 
 $maxPreviewLength = (!empty($modParams['max_preview_length']) ? $modParams['max_preview_length'] : MAX_BLOG_PREVIEW_LENGTH);
+//DEPRECATED Slated for removal -wjames5
+/*
 $user_blog_id = NULL;
 if( count( $ranking['data'] ) ) {
 	$user_blog_id = $ranking['data'][0]['blog_id'];
 }
 $gBitSmarty->assign('user_blog_id', $user_blog_id);
-
+*/
 $gBitSmarty->assign('maxPreviewLength', $maxPreviewLength);
 $gBitSmarty->assign('modLastBlogPosts', $ranking["data"]);
 $gBitSmarty->assign('modLastBlogPostsTitle',(isset($module_params["title"])?$module_params["title"]:""));
--- modules/mod_last_blog_posts.tpl
+++ modules/mod_last_blog_posts.tpl
@@ -1,24 +1,26 @@
-{* $Header: /cvsroot/bitweaver/_bit_blogs/modules/mod_last_blog_posts.tpl,v 1.3 2007/03/22 12:15:34 phoenixandy Exp $ *}
+{* $Header: /cvsroot/bitweaver/_bit_blogs/modules/mod_last_blog_posts.tpl,v 1.4 2007/03/27 21:54:30 wjames5 Exp $ *}
 {strip}
 {if $blogsPackageActive}
 	{bitmodule title="$moduleTitle" name="last_blog_posts"}
 		<ul class="blogs">
 			{section name=ix loop=$modLastBlogPosts}
 				<li class="{cycle values="odd,even"}">
+					<div class="title">{$modLastBlogPosts[ix].title}</div>			
 					<div class="date">{$modLastBlogPosts[ix].created|bit_long_date}
 					<br />
 					by {displayname hash=$modLastBlogPosts[ix]}</div>
 					{$modLastBlogPosts[ix].parsed|truncate:$maxPreviewLength}
-					<br />
 					<a href="{$modLastBlogPosts[ix].post_url}">Read more</a>
 				</li>
 			{sectionelse}
 				<li></li>
 			{/section}
 		</ul>
+		{* DEPRECATED Slated for removal - only legacy sites have user blogs -wjames5
 		{if $user_blog_id}
 			<div style="text-align:center;"><a href="{$smarty.const.BIT_ROOT_URL}blogs/view.php?blog_id={$user_blog_id}">Visit my blog</a></div>
 		{/if}
+		*}
 	{/bitmodule}
 {/if}
 {/strip}
