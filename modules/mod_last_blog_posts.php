9167ff58a5c82a799b5725dedce7dde87ea05d9b
wjames5 <will@tekimaki.com>
2007-03-31 22:39:15 +0000

    accepts params user and blog id to limit list

--- modules/mod_last_blog_posts.php
+++ modules/mod_last_blog_posts.php
@@ -3,7 +3,7 @@
  * Params: 
  * - title : if is "title", show the title of the post, else show the date of creation
  *
- * @version $Header: /cvsroot/bitweaver/_bit_blogs/modules/mod_last_blog_posts.php,v 1.7 2007/03/27 21:54:30 wjames5 Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_blogs/modules/mod_last_blog_posts.php,v 1.8 2007/03/31 22:39:15 wjames5 Exp $
  * @package blogs
  * @subpackage modules
  */
@@ -15,16 +15,19 @@ if( !defined( 'MAX_BLOG_PREVIEW_LENGTH' ) ) {
 	define ('MAX_BLOG_PREVIEW_LENGTH', 100);
 }
 
-//DEPRECATED Slated for removal -wjames5
-//include_once( BLOGS_PKG_PATH.'BitBlog.php' );
 include_once( BLOGS_PKG_PATH.'BitBlogPost.php' );
 require_once( USERS_PKG_PATH.'BitUser.php' );
 
 global $gBitSmarty, $gQueryUserId, $module_rows, $module_params, $gBitSystem;
 
 $listHash = array( 'user_id' => $gQueryUserId, 'sort_mode' => 'created_desc', 'max_records' => $module_rows, 'parse_data' => TRUE );
+
+if ( isset($module_params['user']) ){ $listHash['login'] = $module_params['user']; }
+if ( isset($module_params['id']) ){ $listHash['blog_id'] = $module_params['id'];}
+
 $blogPost = new BitBlogPost();
 $ranking = $blogPost->getList( $listHash );
+
 
 $gBitThemes = new BitThemes();
 $modParams = $gBitThemes->getModuleParameters('bitpackage:blogs/mod_last_blog_posts.tpl', $gQueryUserId);
