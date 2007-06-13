0d71d399f97241c86f2dd15657c25107d55a122d
Max Kremmel <xing@synapse.plus.com>
2007-06-13 20:19:32 +0000

    temporary hack to get services working on this page

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_blogs/index.php,v 1.8 2007/05/16 16:47:07 wjames5 Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_blogs/index.php,v 1.9 2007/06/13 20:19:32 squareing Exp $
 
  * @package blogss
  * @subpackage functions
@@ -17,6 +17,19 @@ require_once( BLOGS_PKG_PATH.'lookup_blog_inc.php');
 
 $gBitSystem->verifyPackage( 'blogs' );
 $gBitSystem->verifyPermission( 'p_blogs_view' );
+
+
+// {{{ start hack
+// this is a very stupid hack until we can sort out the following problem: when 
+// calling center_list_blog_posts.tpl, the php file that goes with it is only 
+// called when the tpl file is called, which is in the middle of the rendering 
+// process i.e. after header_inc.tpl has been rendered.  services such as stars 
+// need to be called before the rendering process that data can be passed on 
+// into their header_inc.tpl file.
+$dummy = array();
+$gContent->getList( $dummy );
+// }}} end hack
+
 
 $gBitSmarty->assign( 'showEmpty', TRUE );
 $gDefaultCenter = 'bitpackage:blogs/center_list_blog_posts.tpl';
