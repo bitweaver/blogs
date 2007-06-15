290056f87f9cc68fa8ef4a34c4cb6155b76db51a
wjames5 <will@tekimaki.com>
2007-06-15 16:15:06 +0000

    fix xing hack to get services loaded up right for center ref on recent posts - still does not solve problm when cetner is used in other parts of site

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_blogs/index.php,v 1.9 2007/06/13 20:19:32 squareing Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_blogs/index.php,v 1.10 2007/06/15 16:15:06 wjames5 Exp $
 
  * @package blogss
  * @subpackage functions
@@ -13,11 +13,14 @@
  * required setup
  */
 require_once( '../bit_setup_inc.php' );
-require_once( BLOGS_PKG_PATH.'lookup_blog_inc.php');
 
+// Is package installed and enabled
 $gBitSystem->verifyPackage( 'blogs' );
+
+// Now check permissions to access this page
 $gBitSystem->verifyPermission( 'p_blogs_view' );
 
+require_once( BLOGS_PKG_PATH.'lookup_blog_inc.php');
 
 // {{{ start hack
 // this is a very stupid hack until we can sort out the following problem: when 
@@ -26,16 +29,16 @@ $gBitSystem->verifyPermission( 'p_blogs_view' );
 // process i.e. after header_inc.tpl has been rendered.  services such as stars 
 // need to be called before the rendering process that data can be passed on 
 // into their header_inc.tpl file.
+$blogPost = new BitBlogPost();
 $dummy = array();
-$gContent->getList( $dummy );
+$blogPosts = $blogPost->getList( $dummy );
 // }}} end hack
 
-
 $gBitSmarty->assign( 'showEmpty', TRUE );
+
+// Display the template
 $gDefaultCenter = 'bitpackage:blogs/center_list_blog_posts.tpl';
 $gBitSmarty->assign_by_ref( 'gDefaultCenter', $gDefaultCenter );
 
-// Display the template
 $gBitSystem->display( 'bitpackage:kernel/dynamic.tpl', 'List Blog Posts' );
-
 ?>
