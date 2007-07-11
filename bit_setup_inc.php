32bee4bdd98081121117cc0e8b9f8b23182a4842
Max Kremmel <xing@synapse.plus.com>
2007-07-11 10:13:54 +0000

    add link to comment options

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -22,15 +22,16 @@ if( $gBitSystem->isPackageActive( 'blogs' ) ) {
 
 	if( $gBitUser->hasPermission( 'p_blogs_view' ) ) {
 		$menuHash = array(
-			'package_name'  => BLOGS_PKG_NAME,
-			'index_url'     => BLOGS_PKG_URL.'index.php',
-			'menu_template' => 'bitpackage:blogs/menu_blogs.tpl',
+			'package_name'       => BLOGS_PKG_NAME,
+			'index_url'          => BLOGS_PKG_URL.'index.php',
+			'menu_template'      => 'bitpackage:blogs/menu_blogs.tpl',
+			'admin_comments_url' => KERNEL_PKG_URL.'admin/index.php?page=blogs',
 		);
 		$gBitSystem->registerAppMenu( $menuHash );
 	}
 
 	$gBitSystem->registerNotifyEvent( array( "blog_post" => tra("An entry is posted to a blog") ) );
 
-	require_once( 'BitBlog.php' );	
+	require_once( 'BitBlog.php' );
 }
 ?>
