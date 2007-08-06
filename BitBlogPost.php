a7e5adb2339454f82dea8a9bd7f38856e708ee1e
Christian Fowler <spider@viovio.com>
2007-08-06 15:31:40 +0000

    allow for NULL entires in publish/expire dates for getList

--- BitBlogPost.php
+++ BitBlogPost.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_blogs/BitBlogPost.php,v 1.83 2007/08/05 21:00:50 spiderr Exp $
+ * $Header: /cvsroot/bitweaver/_bit_blogs/BitBlogPost.php,v 1.84 2007/08/06 15:31:40 spiderr Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: BitBlogPost.php,v 1.83 2007/08/05 21:00:50 spiderr Exp $
+ * $Id: BitBlogPost.php,v 1.84 2007/08/06 15:31:40 spiderr Exp $
  *
  * Virtual base class (as much as one can have such things in PHP) for all
  * derived tikiwiki classes that require database access.
@@ -16,7 +16,7 @@
  *
  * @author drewslater <andrew@andrewslater.com>, spiderr <spider@steelsun.com>
  *
- * @version $Revision: 1.83 $ $Date: 2007/08/05 21:00:50 $ $Author: spiderr $
+ * @version $Revision: 1.84 $ $Date: 2007/08/06 15:31:40 $ $Author: spiderr $
  */
 
 /**
@@ -760,11 +760,10 @@ class BitBlogPost extends LibertyAttachable {
 			$bindVars[] = ( int )$now;
 		} else {
 			// hide future and expired posts
-			$whereSql .= " AND bp.`publish_date` <= ? AND ( bp.`expire_date` <= bp.`publish_date` ) OR ( bp.`expire_date` > ? )";
+			$whereSql .= " AND ((bp.`publish_date` IS NULL AND bp.`expire_date` IS NULL) OR (bp.`publish_date` <= ? AND ( bp.`expire_date` <= bp.`publish_date` ) OR ( bp.`expire_date` > ? )))";
 			$bindVars[] = ( int )$now;
 			$bindVars[] = ( int )$now;
 		}
-		
 
 		if( empty( $pListHash['sort_mode'] ) ) {
 			$pListHash['sort_mode'] = 'publish_date_desc';
