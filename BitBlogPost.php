e028502231f6af8c6842056016389ab0735896b1
wjames5 <will@tekimaki.com>
2007-12-09 04:42:46 +0000

    get hits specifically - we accidentally nulled the content_id

--- BitBlogPost.php
+++ BitBlogPost.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_blogs/BitBlogPost.php,v 1.112 2007/12/08 21:13:28 wjames5 Exp $
+ * $Header: /cvsroot/bitweaver/_bit_blogs/BitBlogPost.php,v 1.113 2007/12/09 04:42:46 wjames5 Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: BitBlogPost.php,v 1.112 2007/12/08 21:13:28 wjames5 Exp $
+ * $Id: BitBlogPost.php,v 1.113 2007/12/09 04:42:46 wjames5 Exp $
  *
  * Virtual base class (as much as one can have such things in PHP) for all
  * derived tikiwiki classes that require database access.
@@ -16,7 +16,7 @@
  *
  * @author drewslater <andrew@andrewslater.com>, spiderr <spider@steelsun.com>
  *
- * @version $Revision: 1.112 $ $Date: 2007/12/08 21:13:28 $ $Author: wjames5 $
+ * @version $Revision: 1.113 $ $Date: 2007/12/09 04:42:46 $ $Author: wjames5 $
  */
 
 /**
@@ -68,7 +68,7 @@ class BitBlogPost extends LibertyAttachable {
 			$this->getServicesSql( 'content_load_sql_function', $selectSql, $joinSql, $whereSql, $bindVars );
 
 			$query = "
-				SELECT bp.*, lc.*, lcds.`data` AS `summary`, lch.*, uu.`login`, uu.`real_name`, lf.`storage_path` as avatar,
+				SELECT bp.*, lc.*, lcds.`data` AS `summary`, lch.`hits`, uu.`login`, uu.`real_name`, lf.`storage_path` as avatar,
 					lfp.storage_path AS `image_attachment_path` 
 					$selectSql
 				FROM `".BIT_DB_PREFIX."blog_posts` bp
@@ -891,7 +891,7 @@ class BitBlogPost extends LibertyAttachable {
 		
 		$query = "
 			SELECT
-				bp.*, lc.*, lch.*, lcds.`data` AS `summary`, COALESCE( bp.`publish_date`, lc.`last_modified` ) AS sort_date,
+				bp.*, lc.*, lch.`hits`, lcds.`data` AS `summary`, COALESCE( bp.`publish_date`, lc.`last_modified` ) AS sort_date,
 				uu.`email`, uu.`login`, uu.`real_name`, ulf.`storage_path` as avatar,  lf.storage_path AS `image_attachment_path`
 				$selectSql
 			FROM `".BIT_DB_PREFIX."blog_posts` bp
