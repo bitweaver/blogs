6485be72a6e7f41b670e92b1ed691aed56ae160c
wjames5 <will@tekimaki.com>
2009-11-22 15:29:44 +0000

    rollback this is not the right way to enforce crossposting as blog is not required for posting

--- post.php
+++ post.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_blogs/post.php,v 1.69 2009/11/22 15:11:49 lsces Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_blogs/post.php,v 1.70 2009/11/22 15:29:44 wjames5 Exp $
 
  * @package blogs
  * @subpackage functions
@@ -111,7 +111,7 @@ if (isset($_REQUEST["preview"])) {
 	}
 } elseif( !empty( $_REQUEST['edit'] ) ) {
 } else {
-	$gBlog->invokeServices( 'content_edit_function' );
+	$gContent->invokeServices( 'content_edit_function' );
 	if( $gContent->isValid() && $gContent->getContentStatus() == -5 && $gContent->getField('publish_date') < $gBitSystem->getUTCTime() ){
 		/* if we are working with a draft and a future publish date is not set 
 		 * then we automatically move the publish date up to NOW to help users from publishing in the past.
